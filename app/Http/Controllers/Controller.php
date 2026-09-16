9fe1900ea95ed19fbf9721112e579e0d9699eb54
Engs-carlospohlod-svg <engs-carlospohlod@camporeal.edu.br>
2026-09-16 20:23:18 -0300

    atv 4

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,42 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Http\Request;
+
+class AlunoController extends Controller
 {
-    //
+    public function index()
+    {
+        return 'Lista de alunos';
+    }
+
+    public function show($id)
+    {
+        return 'Aluno: ' . $id;
+    }
+
+    public function create()
+    {
+        return 'Formulário de cadastro';
+    }
+
+    public function store(Request $request)
+    {
+        return 'Aluno cadastrado';
+    }
+
+    public function edit($id)
+    {
+        return 'Editando aluno: ' . $id;
+    }
+
+    public function update(Request $request, $id)
+    {
+        return 'Aluno atualizado: ' . $id;
+    }
+
+    public function destroy($id)
+    {
+        return 'Aluno excluído: ' . $id;
+    }
 }
