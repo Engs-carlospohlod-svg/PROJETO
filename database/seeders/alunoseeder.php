8a7340f76438166cf85b38d8e250c6795c4124c2
Engs-carlospohlod-svg <engs-carlospohlod@camporeal.edu.br>
2026-09-16 21:26:09 -0300

    atv 12

---
+++ database/seeders/alunoseeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Aluno;
+
+class AlunoSeeder extends Seeder
+{
+    public function run(): void
+    {
+        for ($i = 1; $i <= 10; $i++) {
+            Aluno::create([
+                'nome' => 'Aluno ' . $i,
+                'email' => 'aluno' . $i . '@email.com',
+                'curso' => 'Engenharia de Software',
+            ]);
+        }
+    }
+}
